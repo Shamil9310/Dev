5f248532f2c3dfed8d7269ba52e6ac7c90c56687
Shamil Isaev <Itachi9310@yandex.ru>
2024-05-01 15:25:19 +0300

    roug

--- code design.py
+++ code design.py
@@ -1,24 +1,26 @@
 from math import sqrt
+from typing import Optional
 
 
-def add_numbers(number_one, number_two):
+def add_numbers(number_one: int, number_two: int) -> int:
+    """Возвращяет сумму двух чиссел."""
     return number_one + number_two
 
 
-def сalculatesquareroot(number):
+def calculate_square_root(number: int) -> float:
     return sqrt(number)
 
 
-def calc(your_number):
+def calc(your_number: int) -> Optional[str]:
     if your_number <= 0:
         return
-    return (f'Мы вычислили квадратный корень из введённого вами числа.'
-    f' Это будет: {сalculatesquareroot(your_number)}')
+    return 'Мы вычислили квадратный корень из введённого вами числа. \
+Это будет:', calculate_square_root(your_number)
 
 
-number_x = 10
-number_y = 5
+number_one = 10
+number_two = 5
 
-print('Сумма чисел: ', add_numbers(number_x, number_y))
+print('Сумма чисел:', add_numbers(number_one, number_two))
 
 print(calc(25.5))
